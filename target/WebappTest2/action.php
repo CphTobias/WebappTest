cb74ec467fef3d29224a82220855890470a8594b
Tobiascph <tobias.zimmer@hotmail.com>
2020-10-09 10:15:02 +0200

    Initial commit

---
+++ target/WebappTest2/action.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $from = $_POST['email']; // this is the sender's Email address
+    $to = "email@example.com"; // this is your Email address
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$message,$headers);
+    mail($from,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+}
+?>
